587b47b5398a4d6802a884582f7f956d64940a99
RenatusRS <freekill246@gmail.com>
2022-04-10 16:43:18 +0200

    Dodata baza i ER dijagram

---
+++ faza4/database.sql
@@ -0,0 +1,311 @@
+CREATE DATABASE  IF NOT EXISTS `fludj` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `fludj`;
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: fludj
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bundle`
+--
+
+DROP TABLE IF EXISTS `bundle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bundle` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `discount` int NOT NULL,
+  `description` varchar(4000) NOT NULL,
+  `pic_big_rect` varchar(200) NOT NULL,
+  `pic_small_rect` varchar(200) NOT NULL,
+  `pic_background` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bundle`
+--
+
+LOCK TABLES `bundle` WRITE;
+/*!40000 ALTER TABLE `bundle` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bundle` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bundled`
+--
+
+DROP TABLE IF EXISTS `bundled`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bundled` (
+  `id_bundle` int NOT NULL,
+  `id_product` int NOT NULL,
+  PRIMARY KEY (`id_bundle`,`id_product`),
+  KEY `fk_id_product_b_idx` (`id_product`),
+  CONSTRAINT `fk_id_bundle` FOREIGN KEY (`id_bundle`) REFERENCES `bundle` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_id_product_b` FOREIGN KEY (`id_product`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bundled`
+--
+
+LOCK TABLES `bundled` WRITE;
+/*!40000 ALTER TABLE `bundled` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bundled` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `coupon`
+--
+
+DROP TABLE IF EXISTS `coupon`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `coupon` (
+  `id_owner` int NOT NULL,
+  `id_product` int NOT NULL,
+  `discount` int NOT NULL,
+  PRIMARY KEY (`id_owner`,`id_product`),
+  KEY `fk_id_product_idx` (`id_product`),
+  KEY `fk_id_product_c_idx` (`id_product`),
+  CONSTRAINT `fk_id_owner` FOREIGN KEY (`id_owner`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_id_product_c` FOREIGN KEY (`id_product`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `coupon`
+--
+
+LOCK TABLES `coupon` WRITE;
+/*!40000 ALTER TABLE `coupon` DISABLE KEYS */;
+/*!40000 ALTER TABLE `coupon` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `genre`
+--
+
+DROP TABLE IF EXISTS `genre`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `genre` (
+  `id_product` int NOT NULL,
+  `genre_name` varchar(25) NOT NULL,
+  PRIMARY KEY (`id_product`,`genre_name`),
+  CONSTRAINT `fk_id_product_g` FOREIGN KEY (`id_product`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `genre`
+--
+
+LOCK TABLES `genre` WRITE;
+/*!40000 ALTER TABLE `genre` DISABLE KEYS */;
+/*!40000 ALTER TABLE `genre` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ownership`
+--
+
+DROP TABLE IF EXISTS `ownership`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ownership` (
+  `id_product` int NOT NULL,
+  `id_user` int NOT NULL,
+  `text` varchar(1500) DEFAULT NULL,
+  `rating` int DEFAULT NULL,
+  PRIMARY KEY (`id_product`),
+  KEY `fk_id_user_idx` (`id_user`),
+  CONSTRAINT `fk_id_product` FOREIGN KEY (`id_product`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_id_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ownership`
+--
+
+LOCK TABLES `ownership` WRITE;
+/*!40000 ALTER TABLE `ownership` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ownership` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `price` decimal(10,0) NOT NULL,
+  `base_game` int DEFAULT NULL,
+  `discount` int DEFAULT NULL,
+  `discount_expire` datetime DEFAULT NULL,
+  `description` varchar(4000) NOT NULL,
+  `developer` varchar(30) NOT NULL,
+  `publisher` varchar(30) NOT NULL,
+  `release_date` date NOT NULL,
+  `video` varchar(200) NOT NULL,
+  `pic_big_rect` varchar(200) NOT NULL,
+  `pic_small_rect` varchar(200) NOT NULL,
+  `pic_background` varchar(200) NOT NULL,
+  `pic_vertical` varchar(200) NOT NULL,
+  `pic_ss1` varchar(200) NOT NULL,
+  `pic_ss2` varchar(200) NOT NULL,
+  `pic_ss3` varchar(200) NOT NULL,
+  `os_min` varchar(30) NOT NULL,
+  `ram_min` varchar(30) NOT NULL,
+  `gpu_min` varchar(30) NOT NULL,
+  `cpu_min` varchar(30) NOT NULL,
+  `mem_min` varchar(30) NOT NULL,
+  `os_rec` varchar(30) NOT NULL,
+  `ram_rec` varchar(30) NOT NULL,
+  `gpu_rec` varchar(30) NOT NULL,
+  `cpu_rec` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`),
+  KEY `fk_base_game_idx` (`base_game`),
+  CONSTRAINT `fk_base_game` FOREIGN KEY (`base_game`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `relationships`
+--
+
+DROP TABLE IF EXISTS `relationships`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `relationships` (
+  `id_user1` int NOT NULL,
+  `id_user2` int NOT NULL,
+  `status` tinyint NOT NULL,
+  PRIMARY KEY (`id_user1`,`id_user2`),
+  KEY `fk_id_user2_idx` (`id_user2`),
+  CONSTRAINT `fk_id_user1` FOREIGN KEY (`id_user1`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_id_user2` FOREIGN KEY (`id_user2`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `relationships`
+--
+
+LOCK TABLES `relationships` WRITE;
+/*!40000 ALTER TABLE `relationships` DISABLE KEYS */;
+/*!40000 ALTER TABLE `relationships` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `review_vote`
+--
+
+DROP TABLE IF EXISTS `review_vote`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `review_vote` (
+  `id_user` int NOT NULL,
+  `id_poster` int NOT NULL,
+  `id_product` int NOT NULL,
+  `like` tinyint NOT NULL,
+  PRIMARY KEY (`id_user`,`id_poster`,`id_product`),
+  KEY `fk_id_poster_idx` (`id_poster`),
+  KEY `fk_id_product_rv_idx` (`id_product`),
+  CONSTRAINT `fk_id_poster` FOREIGN KEY (`id_poster`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_id_product_rv` FOREIGN KEY (`id_product`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_id_user_rv` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `review_vote`
+--
+
+LOCK TABLES `review_vote` WRITE;
+/*!40000 ALTER TABLE `review_vote` DISABLE KEYS */;
+/*!40000 ALTER TABLE `review_vote` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `admin_rights` tinyint NOT NULL,
+  `balance` decimal(10,0) NOT NULL,
+  `review_ban` tinyint NOT NULL,
+  `avatar` varchar(200) DEFAULT NULL,
+  `description` varchar(150) DEFAULT NULL,
+  `country` varchar(2) DEFAULT NULL,
+  `real_name` varchar(20) DEFAULT NULL,
+  `nickname` varchar(20) NOT NULL,
+  `featured_review` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  KEY `fk_featured_review_idx` (`featured_review`),
+  CONSTRAINT `fk_featured_review` FOREIGN KEY (`featured_review`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-10 16:21:25
