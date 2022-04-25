a7c5d44f28eb0e975edc738cd967cba8b5d44ce9
RenatusRS <freekill246@gmail.com>
2022-04-25 11:55:18 +0200

    Uklanjanje log fajlova

--- faza5/app/Views/template/header_user.php
+++ faza5/app/Views/template/header_user.php
@@ -4,9 +4,10 @@
     <ul>
         <li><a href=http://localhost:8080/ id="logo"><img src=assets/logo.png height=40px></a></li>
         <li><a href=http://localhost:8080 />STORE</a></li>
+        <li><a href=http://localhost:8080/User/Profile><?php echo $user->username; ?></a></li>
         <li style="float:right"><a href=http://localhost:8080/User/Logout>LOG OUT</a></li>
         <li style="float:right"><a href=http://localhost:8080/User/AddFunds><?php echo number_format($user->balance, 2); ?>$</a></li>
-        <li style="float:right"><a href=http://localhost:8080/User/Profile><?php echo $user->username; ?></a></li>
+
     </ul>
 </header>
 <i class="fa-solid fa-flask-vial"></i>
