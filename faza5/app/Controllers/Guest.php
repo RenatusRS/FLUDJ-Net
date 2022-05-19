17a83ae3afa5817a17ce85241c9fcbb11f384e46
Fedja Mladenovic <fedjamladenovic@gmail.com>
2022-05-19 20:26:48 +0200

    dodato računanje cene kolekcije

--- faza5/app/Controllers/Guest.php
+++ faza5/app/Controllers/Guest.php
@@ -58,4 +58,16 @@ class Guest extends BaseController {
         $this->session->set('user', $user);
         return redirect()->to(site_url("User/Profile/"));
     }
+
+    protected function determineBundlePriceAndDiscount($products, $discount) {
+        $price = 0.0;
+        foreach ($products as $product) {
+            $price += $product->price;
+        }
+
+        return ['price' => $price,
+                'discount' => $discount];
+    }
+
+    public function product($id) {}
 }
