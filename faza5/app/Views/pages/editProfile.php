5742f0112d31ddd9ae704ce4ba40a0298702e1e7
RenatusRS <freekill246@gmail.com>
2022-05-30 22:09:55 +0200

    Update editProfile.php

--- faza5/app/Views/pages/editProfile.php
+++ faza5/app/Views/pages/editProfile.php
@@ -17,41 +17,38 @@ use App\Models\UserM;
 
 $us = (new UserM())->where('id', $user->id)->first();
 ?>
-    <div id=main style="display:flex;flex-wrap: wrap;">
-        <div style="flex:100%; padding: 0 16px">
-            <?php if (!empty($errors)) : ?>
-                <div style='color:red;'>
-                    <?php foreach ($errors as $field => $error) : ?>
-                        <p><?= $error ?></p>
-                    <?php endforeach ?>
-                </div>
-            <?php endif ?>
+
+<div id="main" style="margin: 100px auto; width: 325px; padding: 15px; border-radius: 9px;">
+    <?php if (!empty($errors)) : ?>
+        <div style='color:red;'>
+            <?php foreach ($errors as $field => $error) : ?>
+                <p><?= $error ?></p>
+            <?php endforeach ?>
         </div>
+    <?php endif ?>
+    <form name='friend_button' action="<?= site_url("user/EditProfileSubmit/") ?>" method="POST" enctype="multipart/form-data">
 
-    <div id="main" style="margin: 100px auto; width: 325px; padding: 15px; border-radius: 9px;">
-        <form name='friend_button' action="<?= site_url("user/EditProfileSubmit/") ?>" method="POST" enctype="multipart/form-data">
+        <h3>Avatar</h3>
+        <input type="file" name="profile_pic" accept="image/jpeg">
 
-            <h3>Avatar</h3>
-            <input type="file" name="profile_pic" accept="image/jpeg">
+        <h3>Nickname</h3>
+        <input type="text" name="nickname" class="full" value="<?= set_value('nickname', $us->nickname); ?>" />
 
-            <h3>Nickname</h3>
-            <input type="text" name="nickname" class="full" value="<?= set_value('nickname', $us->nickname); ?>" />
-            
-            <h3>Real Name</h3>
-            <input type="text" name="real_name" class="full" value="<?= set_value('real_name', $us->real_name); ?>" />
+        <h3>Real Name</h3>
+        <input type="text" name="real_name" class="full" value="<?= set_value('real_name', $us->real_name); ?>" />
 
-            <h3>Description</h3>
-            <textarea name="description" rows=10 style="width:100%; max-width: 100%; min-width: 100%;"><?php echo $us->description; ?></textarea>
+        <h3>Description</h3>
+        <textarea name="description" rows=10 style="width:100%; max-width: 100%; min-width: 100%;"><?php echo $us->description; ?></textarea>
 
-            <h3>Featured Review</h3>
-            <?php $os = (new OwnershipM())->where('id_user', $us->id)->findAll(); ?>
-            <select name="f_review" id="feat_reviews">
-                <?php foreach ($os as $osr) {
-                    $pr = (new ProductM())->where('id', $osr->id_product)->first(); ?>
-                    <option value="<?php echo $pr->id ?>"><?php echo $pr->name ?></option>
-                <?php } ?>
-            </select>
+        <h3>Featured Review</h3>
+        <?php $os = (new OwnershipM())->where('id_user', $us->id)->findAll(); ?>
+        <select name="f_review" id="feat_reviews">
+            <?php foreach ($os as $osr) {
+                $pr = (new ProductM())->where('id', $osr->id_product)->first(); ?>
+                <option value="<?php echo $pr->id ?>"><?php echo $pr->name ?></option>
+            <?php } ?>
+        </select>
 
-            <input type="submit" name="editbtn" class="btn" value=<?= "Edit" ?>>
-        </form>
-    </div>
+        <input type="submit" name="editbtn" class="btn" value=<?= "Edit" ?>>
+    </form>
+</div>
