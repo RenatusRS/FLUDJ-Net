f2cdb360cb0913dca300ec0a3dd63589e79f171c
Fedja Mladenovic <fedjamladenovic@gmail.com>
2022-06-09 00:54:28 +0200

    sklonjeni nepotrebni metodi

--- faza5/app/Models/BundledProductsM.php
+++ faza5/app/Models/BundledProductsM.php
@@ -26,18 +26,6 @@ class BundledProductsM extends Model {
     public $db;
 
     /**
-     * ukloni proizvod sa id-jem $idProduct iz kolekcije sa id-jem $idBundle
-     *
-     * @param  integer $idBundle
-     * @param  integer $idProduct
-     * @return void
-     */
-    public static function removeFromBundle($idBundle, $idProduct) {
-        (new BundledProductsM())->where('id_bundle', $idBundle)
-            ->where('id_product', $idProduct)
-            ->delete();
-    }
-    /**
      * otklanja sve proizvode iz kolekcije sa id-jem $idBundle
      *
      * @param  integer $idBundle
@@ -60,40 +48,6 @@ class BundledProductsM extends Model {
             'id_product' => $idProduct
         ]);
     }
-    /**
-     * find products from bundle with id $id
-     *
-     * @param  integer $id
-     * @return array array of id-s of found products
-     */
-    public function findBundledProducts($id) { // TODO ovo je isti kod kao traženje žanrova. napraviti u nadklasi apstrakciju ova dva
-        $rows = $this->where($this->primaryKey, $id)
-            ->findAll();
-
-        $result = [];
-        foreach ($rows as $row) {
-            array_push($result, $row->id_product);
-        }
-
-        return $result;
-    }
-
-    /**
-     * proverava da li je proizvod sa id-jem $productId u kolekciji
-     * sa id-jem $bundleId
-     *
-     * @param  integer $bundleId
-     * @param  integer $productId
-     * @return bool
-     */
-    public function inBundle($idBundle, $idProduct) {
-        $query = $this->where('id_product', $idProduct)
-            ->where('id_bundle', $idBundle)
-            ->first();
-
-        return (isset($query));
-    }
-
     /**
      * vrati sve proizvode koji se nalaze u kolekciji sa id-jem $idBundle
      *
